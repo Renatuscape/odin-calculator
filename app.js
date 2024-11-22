6ac6e0b1db4e6811ede22af95d6c312eef70987e
Renatuscape <silarcta@gmail.com>
2024-11-22 15:11:18 +0100

    Adds variables to hold input, and function to print calculator number buttons

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const displayWindow = document.getElementById('display-window');
+const numBtnContainer = document.getElementById('num-buttons');
+
 let displayText = 0;
 
-function setDisplay(textContent) {
-    displayWindow.innerHTML  = textContent;
+let input1 = '';
+let input2 = '';
+let operator = '';
+
+function buildButtons() {
+    for (let i = 0; i < 10; i++) {
+        const button = document.createElement("button");
+        button.className = 'calc-button';
+        button.title = i.toString();
+        button.innerHTML = i.toString();
+
+        numBtnContainer.appendChild(button);
+    }
 }
 
-setDisplay(displayText);
+function setDisplay() {
+    let textContent = 0;
+
+    if (input1 !== '') {
+        textContent = input1;
+
+        if (operator !== '') {
+            textContent += ' ' + operator;
+
+            if (input2 !== '') {
+                textContent += ' ' + input2;
+            }
+        }
+    }
+
+    displayWindow.innerHTML = textContent;
+}
+
+buildButtons();
+setDisplay();
