6903d5e495a78ad2215e0d23f428470fb24828b3
Renatuscape <silarcta@gmail.com>
2024-11-28 10:09:03 +0100

    Adds decimal function. Refactors all code using prettier extension

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const numBtnContainer = document.getElementById("num-buttons");
 const operationBtnContainer = document.getElementById("operation-buttons");
 const clearBtn = document.getElementById("clear-button");
 const sumBtn = document.getElementById("sum-button");
-const numpadLayout = [
-    '7', '8', '9',
-    '4', '5', '6',
-    '1', '2', '3',
-    '0'
-];
+const decimalBtn = document.getElementById("decimal-button");
+
+const numpadLayout = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
 
 let displayText = 0;
 
@@ -19,136 +16,155 @@ let operatorName = "";
 let summedUp = false;
 
 const operators = [
-    {
-        operator: "+",
-        action: add,
-    },
-    {
-        operator: "-",
-        action: subtract,
-    },
-    {
-        operator: "x",
-        action: multiply,
-    },
-    {
-        operator: "/",
-        action: divide,
-    },
+  {
+    operator: "+",
+    action: add,
+  },
+  {
+    operator: "-",
+    action: subtract,
+  },
+  {
+    operator: "x",
+    action: multiply,
+  },
+  {
+    operator: "/",
+    action: divide,
+  },
 ];
 
 function sum() {
-    console.log("Chosen operator is " + operatorName);
-    const foundOperation = operators.find(
-        (opr) => opr.operator == operatorName
-    )?.action;
+  console.log("Chosen operator is " + operatorName);
+  const foundOperation = operators.find(
+    (opr) => opr.operator == operatorName
+  )?.action;
 
-    if (foundOperation) {
-        foundOperation();
-    } else {
-        console.error("Operator not found");
-    }
+  if (foundOperation) {
+    foundOperation();
+  } else {
+    console.warn("Operator not set");
+  }
 
-    setDisplay();
-    summedUp = true;
+  setDisplay();
+  summedUp = true;
+}
+
+function decimal() {
+  if (input1 === "") {
+    input1 = "0.";
+  } else if (operatorName === "" && !input1.includes(".")) {
+    input1 += "."
+  }
+  else if (operatorName !== "" && input2 === "") {
+    input2 = "0.";
+  }
+  else if (operatorName !== "" && !input2.includes(".")) {
+    input2 += ".";
+  }
+  setDisplay();
 }
 
 function add() {
-    let result = parseInt(input1) + parseInt(input2);
-    clear();
-    input1 = result.toString();
+  let result = parseFloat(input1) + parseFloat(input2);
+  clear();
+  input1 = result.toString();
 }
 
 function subtract() {
-    let result = parseInt(input1) - parseInt(input2);
-    clear();
-    input1 = result.toString();
+  let result = parseFloat(input1) - parseFloat(input2);
+  clear();
+  input1 = result.toString();
 }
 
 function multiply() {
-    let result = parseInt(input1) * parseInt(input2);
-    clear();
-    input1 = result.toString();
+  let result = parseFloat(input1) * parseFloat(input2);
+  clear();
+  input1 = result.toString();
 }
 
 function divide() {
-    let result = parseInt(input1) / parseInt(input2);
-    clear();
-    input1 = result.toString();
+  let result = parseFloat(input1) / parseFloat(input2);
+  clear();
+  input1 = result.toString();
 }
 
 function clear() {
-    input1 = "";
-    input2 = "";
-    operatorName = "";
-    setDisplay();
+  input1 = "";
+  input2 = "";
+  operatorName = "";
+  setDisplay();
 }
 
 function buildButtons() {
-    numpadLayout.forEach(num => {
-        const button = document.createElement("button");
-        button.className = "calc-button";
-        button.title = num;
-        button.innerHTML = num;
-        button.onclick = () => {
+  numpadLayout.forEach((num) => {
+    const button = document.createElement("button");
+    button.className = "calc-button";
+    button.title = num;
+    button.innerHTML = num;
 
-            if (summedUp) {
-                input1 = '';
-                summedUp = false;
-            }
-
-            if (operatorName === "") {
-                input1 += num;
-            } else {
-                input2 += num;
-            }
-            setDisplay();
-        };
-        numBtnContainer.appendChild(button);
-    });
-
-
-    for (let i = 0; i < operators.length; i++) {
-        const button = document.createElement("button");
-        button.className = "calc-button";
-        button.title = operators[i].operator;
-        button.innerHTML = operators[i].operator;
-        operationBtnContainer.appendChild(button);
-
-        button.onclick = () => {
-            summedUp = false;
-            operatorName = button.title;
-            setDisplay();
-        };
+    if (num === '0') {
+        button.style.gridColumn = "span 3";
     }
+    
+    button.onclick = () => {
+      if (summedUp) {
+        input1 = "";
+        summedUp = false;
+      }
 
-    sumBtn.onclick = sum;
-    clearBtn.onclick = clear;
+      if (operatorName === "") {
+        input1 += num;
+      } else {
+        input2 += num;
+      }
+      setDisplay();
+    };
+    numBtnContainer.appendChild(button);
+  });
+
+  for (let i = 0; i < operators.length; i++) {
+    const button = document.createElement("button");
+    button.className = "calc-button";
+    button.title = operators[i].operator;
+    button.innerHTML = operators[i].operator;
+    operationBtnContainer.appendChild(button);
+
+    button.onclick = () => {
+      summedUp = false;
+      operatorName = button.title;
+      setDisplay();
+    };
+  }
+
+  sumBtn.onclick = sum;
+  clearBtn.onclick = clear;
+  decimalBtn.onclick = decimal;
 }
 
 function setDisplay() {
-    let textContent = 0;
+  let textContent = 0;
 
-    if (input1 !== "") {
-        textContent = Number(input1.replace(/,/g, '')).toLocaleString();
+  if (input1 !== "") {
+    textContent = Number(input1.replace(/,/g, "")).toLocaleString();
 
-        if (operatorName !== "") {
-            textContent += " " + operatorName;
+    if (operatorName !== "") {
+      textContent += " " + operatorName;
 
-            if (input2 !== "") {
-                textContent += " " + Number(input2.replace(/,/g, '')).toLocaleString();
-            }
-        }
+      if (input2 !== "") {
+        textContent += " " + Number(input2.replace(/,/g, "")).toLocaleString();
+      }
     }
+  }
 
-    if (textContent.length > 54) {
-        textContent = "ERROR!";
-    }
-    displayWindow.innerHTML = textContent;
+  if (textContent.length > 54) {
+    textContent = "ERROR!";
+  }
+  displayWindow.innerHTML = textContent;
 
-    console.log("Input1: ", input1);
-    console.log("Operator: ", operatorName);
-    console.log("Input2: ", input2);
+  console.log("Input1: ", input1);
+  console.log("Operator: ", operatorName);
+  console.log("Input2: ", input2);
 }
 
 buildButtons();
